1a01ebf52790f363ea2ea9f31f4047f045299da2
andreymashko7 <63639567+andreymashko7@users.noreply.github.com>
2021-08-05 10:07:42 +0300

    Updates

---
+++ precache-manifest.a93a9d1e472284283cc2da45339ed184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "da51acdbcaf07503a8f8",
+    "url": "/todos-react-hooks/static/css/main.65f8f8f6.chunk.css"
+  },
+  {
+    "revision": "da51acdbcaf07503a8f8",
+    "url": "/todos-react-hooks/static/js/main.4f1b1198.chunk.js"
+  },
+  {
+    "revision": "bb6b7f4fa22808905ce0",
+    "url": "/todos-react-hooks/static/js/runtime~main.d710fd54.js"
+  },
+  {
+    "revision": "551b974e5cbcb0d69952",
+    "url": "/todos-react-hooks/static/css/2.617f8d7b.chunk.css"
+  },
+  {
+    "revision": "551b974e5cbcb0d69952",
+    "url": "/todos-react-hooks/static/js/2.044de0e1.chunk.js"
+  },
+  {
+    "revision": "6f1ca525ee3c80500f217c973e467b4a",
+    "url": "/todos-react-hooks/index.html"
+  }
+];
